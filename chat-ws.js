66a88d76a631b965266d49cae4deda0bdeea518a
c5apple <c5apple_party@yahoo.co.jp>
2022-09-20 16:20:45 +0900

    initial commit

---
+++ chat-ws.js
@@ -0,0 +1,14 @@
+var server = require("ws").Server;
+var s = new server({ port: 5000 });
+s.on("connection", (ws) => {
+  ws.on("message", (msg) => {
+    console.log("Recv: " + msg);
+    s.clients.forEach((client) => {
+      console.log("Send: " + msg);
+      client.send(msg.toString());
+    });
+  });
+  ws.on("close", () => {
+    console.log("Closed.");
+  });
+});
